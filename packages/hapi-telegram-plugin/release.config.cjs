66a3a05851c2ac60e823835316a544a8867163fd
Giacomo Debidda <giacomo@giacomodebidda.com>
2022-07-12 00:31:45 +0200

    chore: update commitlint and semantic-release config

--- packages/hapi-telegram-plugin/release.config.cjs
+++ packages/hapi-telegram-plugin/release.config.cjs
@@ -18,8 +18,9 @@ const git = [
 
 const config = {
   ...base_config,
-  branches: [...base_config.branches],
   plugins: [...base_config.plugins, git]
 }
+
+// console.log('=== semantic-release (hapi-telegram-plugin) ===', config)
 
 module.exports = config
