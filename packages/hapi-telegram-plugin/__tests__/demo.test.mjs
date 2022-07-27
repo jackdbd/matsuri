e777f3df456e1bc1a095137b5b0d982510414b88
Giacomo Debidda <giacomo@giacomodebidda.com>
2022-07-27 11:02:32 +0200

    test(hapi-telegram-plugin): add tests

--- packages/hapi-telegram-plugin/__tests__/demo.test.mjs
+++ packages/hapi-telegram-plugin/__tests__/demo.test.mjs
@@ -1,6 +1,6 @@
 import { testServer } from './utils.mjs'
 
-describe('GET /', () => {
+describe('Hapi app', () => {
   let server
   const timeout_ms = 10000
 
@@ -16,11 +16,57 @@ describe('GET /', () => {
     await server.stop()
   }, timeout_ms)
 
+  // no Telegram message will be delivered to the chat, since this is a success
   it('responds with 200', async () => {
     const res = await server.inject({
       method: 'GET',
-      url: '/'
+      url: '/success'
     })
+
+    expect(res.statusCode).toBe(200)
+    expect(res.result.message).toBe('all good')
+  })
+
+  // a Telegram message will be delivered to the chat, since this is an error
+  it('responds with 500', async () => {
+    const res = await server.inject({
+      method: 'GET',
+      url: '/internal-error'
+    })
+
+    expect(res.statusCode).toBe(500)
+    expect(res.result.message).toBe('An internal server error occurred')
+  })
+
+  // a Telegram message will be delivered to the chat, since this is an error
+  it('responds with 404', async () => {
+    const res = await server.inject({
+      method: 'GET',
+      url: '/nonexistent-path'
+    })
+
     expect(res.statusCode).toBe(404)
+    expect(res.result.message).toBe('Not Found')
+  })
+
+  // a Telegram message will be delivered to the chat, since this is an error
+  it('responds with 401', async () => {
+    const res = await server.inject({
+      method: 'GET',
+      url: '/private'
+    })
+
+    expect(res.statusCode).toBe(401)
+    expect(res.result.message).toBe('Unauthorized')
+  })
+
+  it('responds with 418', async () => {
+    const res = await server.inject({
+      method: 'GET',
+      url: '/teapot'
+    })
+
+    expect(res.statusCode).toBe(418)
+    expect(res.result.message).toBe(`I'm a teapot`)
   })
 })
--- packages/hapi-telegram-plugin/__tests__/utils.mjs
+++ packages/hapi-telegram-plugin/__tests__/utils.mjs
@@ -1,8 +1,48 @@
+import fs from 'node:fs'
+import path from 'node:path'
 import Hapi from '@hapi/hapi'
+import Boom from '@hapi/boom'
 import plugin from '../lib/index.js'
+
+const package_json_path = path.join('package.json')
+const pkg = JSON.parse(fs.readFileSync(package_json_path))
 
 export const testServer = async () => {
   const server = Hapi.server({ port: 8080 })
+
+  server.route({
+    method: 'GET',
+    path: '/success',
+    handler: (request, h) => {
+      return { message: 'all good' }
+    }
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/internal-error',
+    handler: (request, h) => {
+      throw new Error(
+        'this is not the message you will see in the HTTP response'
+      )
+    }
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/private',
+    handler: (request, h) => {
+      throw Boom.unauthorized()
+    }
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/teapot',
+    handler: (request, h) => {
+      throw Boom.teapot()
+    }
+  })
 
   const json = process.env.TELEGRAM
   const { chat_id, token } = JSON.parse(json)
@@ -10,6 +50,9 @@ export const testServer = async () => {
   await server.register({
     plugin,
     options: {
+      app_human_readable_name: 'Hapi-Telegram-Plugin test app',
+      app_technical_name: pkg.name,
+      app_version: pkg.version,
       chat_id,
       token
     }
